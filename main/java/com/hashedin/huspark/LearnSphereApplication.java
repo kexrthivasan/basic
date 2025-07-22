a05948e9c6bb434340e9035c5f3d628a2f7bfb93
kexrthivasan <keerthivasangs10@gmail.com>
2025-07-22 20:37:17 +0530

    commit

---
+++ main/java/com/hashedin/huspark/LearnSphereApplication.java
@@ -0,0 +1,12 @@
+
+package com.hashedin.huspark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSphereApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSphereApplication.class, args);
+    }
+}
